5265e02a27bb3a89eba4b52b1587abc0fabc7118
haodongdong@new3s.com.cn <Hdd15229396450>
2024-02-21 13:48:46 +0800

    修复HashMap处理链表不完整，添加树结构打印

--- src/main/java/cn/map/MyHashMap.java
+++ src/main/java/cn/map/MyHashMap.java
@@ -71,6 +71,7 @@ public class MyHashMap<K extends Comparable<K>,V> {
             }else if(node instanceof TreeNode){
 
                   ((TreeNode<K, V>) node).add(k, v, hashCode, null);
+                  new TreePrintUtil().show(((TreeNode<K, V>) node).root);
 
             }else{
 
@@ -131,10 +132,12 @@ public class MyHashMap<K extends Comparable<K>,V> {
                 treeNode.add(node.key, node.value, node.hash, node.next);
                 node = node.next;
 
-            }while (node.next != null);
+            }while (node != null);
 
             /** 更新数组指向为红黑树 */
             tab[index] = treeNode;
+
+            new TreePrintUtil().show(treeNode.root);
 
         }
 
@@ -258,6 +261,9 @@ public class MyHashMap<K extends Comparable<K>,V> {
 
                 TreeNode<K, V> treeNode = ((TreeNode) node);
                 treeNode.remove(k);
+
+                new TreePrintUtil().show(treeNode.root);
+
                 tab[i] = treeNode;
                 return node.value;
 
@@ -362,6 +368,46 @@ public class MyHashMap<K extends Comparable<K>,V> {
         TreeNode<K, V> right;
 
         TreeNode<K, V> root;
+
+        public boolean isRed() {
+            return red;
+        }
+
+        public void setRed(boolean red) {
+            this.red = red;
+        }
+
+        public TreeNode<K, V> getParent() {
+            return parent;
+        }
+
+        public void setParent(TreeNode<K, V> parent) {
+            this.parent = parent;
+        }
+
+        public TreeNode<K, V> getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode<K, V> left) {
+            this.left = left;
+        }
+
+        public TreeNode<K, V> getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode<K, V> right) {
+            this.right = right;
+        }
+
+        public TreeNode<K, V> getRoot() {
+            return root;
+        }
+
+        public void setRoot(TreeNode<K, V> root) {
+            this.root = root;
+        }
 
         public void add(K k, V v, Integer hash, Node<K,V> next){
 
@@ -937,6 +983,89 @@ public class MyHashMap<K extends Comparable<K>,V> {
 
         }
         
+    }
+
+    /**
+     * 红黑树输出
+     */
+    class TreePrintUtil {
+          /*
+        树的结构示例：
+                  1
+                /   \
+              2       3
+             / \     / \
+            4   5   6   7
+        */
+
+        // 用于获得树的层数
+        public int getTreeDepth(TreeNode root) {
+            return root == null ? 0 : (1 + Math.max(getTreeDepth(root.getLeft()), getTreeDepth(root.getRight())));
+        }
+
+
+        private void writeArray(TreeNode currNode, int rowIndex, int columnIndex, String[][] res, int treeDepth) {
+            // 保证输入的树不为空
+            if (currNode == null) return;
+            // 先将当前节点保存到二维数组中
+            res[rowIndex][columnIndex] = String.valueOf(currNode.value +  (currNode.red ? "R" : "B") + "");
+
+            // 计算当前位于树的第几层
+            int currLevel = ((rowIndex + 1) / 2);
+            // 若到了最后一层，则返回
+            if (currLevel == treeDepth) return;
+            // 计算当前行到下一行，每个元素之间的间隔（下一行的列索引与当前元素的列索引之间的间隔）
+            int gap = treeDepth - currLevel - 1;
+
+            // 对左儿子进行判断，若有左儿子，则记录相应的"/"与左儿子的值
+            if (currNode.getLeft() != null) {
+                res[rowIndex + 1][columnIndex - gap] = "/";
+                writeArray(currNode.getLeft(), rowIndex + 2, columnIndex - gap * 2, res, treeDepth);
+            }
+
+            // 对右儿子进行判断，若有右儿子，则记录相应的"\"与右儿子的值
+            if (currNode.getRight() != null) {
+                res[rowIndex + 1][columnIndex + gap] = "\\";
+                writeArray(currNode.getRight(), rowIndex + 2, columnIndex + gap * 2, res, treeDepth);
+            }
+        }
+
+
+        public void show(TreeNode root) {
+            if (root == null) System.out.println("EMPTY!");
+            // 得到树的深度
+            int treeDepth = getTreeDepth(root);
+
+            // 最后一行的宽度为2的（n - 1）次方乘3，再加1
+            // 作为整个二维数组的宽度
+            int arrayHeight = treeDepth * 2 - 1;
+            int arrayWidth = (2 << (treeDepth - 2)) * 3 + 1;
+            // 用一个字符串数组来存储每个位置应显示的元素
+            String[][] res = new String[arrayHeight][arrayWidth];
+            // 对数组进行初始化，默认为一个空格
+            for (int i = 0; i < arrayHeight; i ++) {
+                for (int j = 0; j < arrayWidth; j ++) {
+                    res[i][j] = " ";
+                }
+            }
+
+            // 从根节点开始，递归处理整个树
+            // res[0][(arrayWidth + 1)/ 2] = (char)(root.val + '0');
+            writeArray(root, 0, arrayWidth/ 2, res, treeDepth);
+
+            // 此时，已经将所有需要显示的元素储存到了二维数组中，将其拼接并打印即可
+            for (String[] line: res) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < line.length; i ++) {
+                    sb.append(line[i]);
+                    if (line[i].length() > 1 && i <= line.length - 1) {
+                        i += line[i].length() > 4 ? 2: line[i].length() - 1;
+                    }
+                }
+                System.out.println(sb);
+            }
+        }
+
     }
 
 }
